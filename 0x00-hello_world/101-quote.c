be417d83c75fcbdca6cdebaf2622075f0fa17abf
Amavic <amarachi5695@gmail.com>
2023-08-10 20:41:40 -0700

    minor changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, text, strlen(text));
 	return (1);
 }
