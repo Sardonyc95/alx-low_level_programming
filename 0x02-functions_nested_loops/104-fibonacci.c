6c6c4efa9885a84aade48825b86fbcd1ebb64467
Amavic <amarachi5695@gmail.com>
2023-08-16 14:09:57 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,6 +13,7 @@ int main(void)
 	unsigned long int nul2;
 	unsigned long int mul1;
 	unsigned long int mul2;
+	unsigned long int I = 1000000000;
 
 	printf("%lu", nul);
 
@@ -23,15 +24,15 @@ int main(void)
 		nul = mul - nul;
 	}
 
-	nul1 = (nul / 1);
-	nul2 = (nul % 1);
-	mul1 = (mul / 1);
-	mul2 = (mul % 1);
+	nul1 = (nul / I);
+	nul2 = (nul % I);
+	mul1 = (mul / I);
+	mul2 = (mul % I);
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", mul1 + (mul2 / 1));
-		printf("%lu", mul2 % 1);
+		printf(", %lu", mul1 + (mul2 / I));
+		printf("%lu", mul2 % I);
 		mul1 = mul1 + nul1;
 		mul1 = mul1 - nul1;
 		mul2 = mul2 + nul2;
