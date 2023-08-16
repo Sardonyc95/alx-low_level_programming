f59d901887101d31bd47c41e79450cfb1e223ae8
Amavic <amarachi5695@gmail.com>
2023-08-16 12:28:28 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: fibonacci <3. no hardcode
+ * Return: (success)
+ */
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int nul = 1;
+	unsigned long int mul = 2;
+	unsigned long int nul1;
+	unsigned long int nul2;
+	unsigned long int mul1;
+	unsigned long int mul2;
 
+	printf("%lu", nul);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", mul);
+		mul += nul;
+		nul = mul - nul;
+	}
+
+	nul1 = (nul / 1);
+	nul2 = (nul % 1);
+	mul1 = (mul / 1);
+	mul2 = (mul % 1);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", mul1 + (mul2 / 1));
+		printf("%lu", mul2 % 1);
+		mul1 = mul1 + nul1;
+		mul1 = mul1 - nul1;
+		mul2 = mul2 + nul2;
+		nul2 = mul2 - nul2;
+	}
+	printf("\n");
+	return (0);
+}
