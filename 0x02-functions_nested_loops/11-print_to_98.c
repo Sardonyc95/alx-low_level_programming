e7c39e4c39ac9095c2cbe718bc3df81f394b6e9f
Amavic <amarachi5695@gmail.com>
2023-08-15 21:47:31 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,15 +13,9 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-			{
-				_putchar(i);
-			}
+				printf("%d, ", i);
 			else if (i == 98)
-			{
-				_putchar(i);
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf("%d\n", i);
 		}
 	}
 	else if (n >= 98)
@@ -29,17 +23,10 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-			{
-				_putchar(j);
-			}
+				printf("%d, ", j);
 			else if (j == 98)
-			{
-				_putchar(j);
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf("%d\n", j);
 		}
 	}
-	_putchar('\n');
 }
 
