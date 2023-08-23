b01ce0e3370990da0586a9ad5feb771ba347fd33
Amavic <amarachi5695@gmail.com>
2023-08-23 14:00:50 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - Function that copy a string
+ * @dest: input param
+ * @src: input param
+ * @n: input param
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	a = 0;
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
