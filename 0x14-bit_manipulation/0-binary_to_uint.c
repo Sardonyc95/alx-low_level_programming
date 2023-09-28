b3246cfd6dc9ee38185a8d3a5039cc0889540477
Amavic <amarachi5695@gmail.com>
2023-09-28 16:21:52 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - Function that converts a binary number to unsigned int
+ * binary_to_uint - Function that converts a binary number to unsigned int
  * @b: input param (string containing the binary number)
  * Return: the converted number
  */
