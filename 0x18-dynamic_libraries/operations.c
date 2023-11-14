360f15413f29363f9167f6311fba61f5ef72e608
Amavic <amarachi5695@gmail.com>
2023-11-14 10:08:02 -0800

    Let call C functions from Python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: input param one
+ * @b: input param two
+ *
+ * Return: final result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: input param 1
+ * @b: input param 2
+ *
+ * Return: final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: input param 1
+ * @b: input param 2
+ *
+ * Return: final result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two integers
+ * @a: input param 1
+ * @b: input param 2
+ *
+ * Return: final result
+ */
+int div_digit(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod of two integers
+ * @a: input param 1
+ * @b: input param 2
+ *
+ * Return: final result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
